aa54f1187486d60f88a0e23a31be2fff8df2e3c0
tromesounds <tromesounds@gmail.com>
2025-09-23 21:43:19 +0100

    Update vite.config.js
    changed base for subdomain compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/threejs-project_website1/',
+  base: '/',
   assetsInclude: ['**/*.exr', '**/*.json'],
   build: {
     chunkSizeWarningLimit: 600 // kB
