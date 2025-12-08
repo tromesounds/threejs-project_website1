c9cb2f4a005ca75e4db00ade56359af907d77119
tromesounds <tromesounds@gmail.com>
2025-12-08 19:37:49 +0000

    Trigger rebuild

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   base: '/threejs-project_website1/',
   assetsInclude: ['**/*.exr', '**/*.json'],
   build: {
-    chunkSizeWarningLimit: 600 // kB
+    chunkSizeWarningLimit: 600 // kB 
   }
 });
